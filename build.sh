e5ab85d0a9b2bcf7c3fb3e5d7af0ffe95b91e048
CodingSpiderFox <codingspiderfox@gmail.com>
2019-12-09 21:51:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y uuid-dev libndp-dev libsystemd-dev libjansson-dev libselinux1-dev libaudit-dev libpolkit-gobject-1-dev ppp-dev libmm-glib-dev libpsl-dev libcurl4-openssl-dev libnewt-dev libqt4-dev
+sudo apt install -y uuid-dev libndp-dev libsystemd-dev libjansson-dev libselinux1-dev libaudit-dev libpolkit-gobject-1-dev ppp-dev libmm-glib-dev libpsl-dev libcurl4-openssl-dev libnewt-dev libqt4-dev libreadline-dev
 meson build --prefix=/usr  
 cd build
 ninja
